5b00e9e8b982f4e2a735f705ce5143c6f202fee5
Dom Slee <domslee97@gmail.com>
2018-08-18 18:41:25 +1000

    Add sha256

--- Formula/give_ssh.rb
+++ Formula/give_ssh.rb
@@ -3,6 +3,7 @@ class GiveSsh < Formula
   homepage "https://github.come/domsleee/give_ssh"
 
   url "https://github.com/domsleee/give_ssh/archive/0.02.tar.gz", :using => :curl
+  sha256 "bcf51fa73fac8b295c35c4abf997906a06e0675a4531f32aa777516c57a82e85"
 
   def install
     bin.install "give_ssh"
