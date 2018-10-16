6527d15394500fccf0321456748eec8de1e8e03d
Dom Slee <domslee97@gmail.com>
2018-10-17 00:10:56 +1100

    upgrade to 0.03

--- Formula/give_ssh.rb
+++ Formula/give_ssh.rb
@@ -2,8 +2,8 @@ class GiveSsh < Formula
   desc "ssh interface for give."
   homepage "https://github.come/domsleee/give_ssh"
 
-  url "https://github.com/domsleee/give_ssh/archive/0.02.tar.gz", :using => :curl
-  sha256 "bcf51fa73fac8b295c35c4abf997906a06e0675a4531f32aa777516c57a82e85"
+  url "https://github.com/domsleee/give_ssh/archive/0.03.tar.gz", :using => :curl
+  sha256 "e92df3a37b0728025acc82c0afef4c39862a63dc3007a1dda0d79cc2745e2e3f"
 
   def install
     bin.install "give_ssh"
